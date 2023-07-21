43cd5967dcbc749345463c0ed3611fe42b15b73a
yuxibenxi <cyu245656@gmail.com>
2023-07-21 12:50:35 +0800

    update SDK to 1.20.11.01

---
+++ SDK/include/llapi/mc/InsidePowderSnowBlockSystem.hpp
@@ -0,0 +1,28 @@
+/**
+ * @file  InsidePowderSnowBlockSystem.hpp
+ *
+ */
+#pragma once
+#define AUTO_GENERATED
+#include "llapi/Global.h"
+
+#define BEFORE_EXTRA
+
+#undef BEFORE_EXTRA
+
+
+namespace InsidePowderSnowBlockSystem {
+
+#define AFTER_EXTRA
+
+#undef AFTER_EXTRA
+    /**
+     * @symbol ?createClientSideSpawnParticleSystem\@InsidePowderSnowBlockSystem\@\@YA?AUTickingSystemWithInfo\@\@XZ
+     */
+    MCAPI struct TickingSystemWithInfo createClientSideSpawnParticleSystem();
+    /**
+     * @symbol ?createServerSideClearFireSystem\@InsidePowderSnowBlockSystem\@\@YA?AUTickingSystemWithInfo\@\@XZ
+     */
+    MCAPI struct TickingSystemWithInfo createServerSideClearFireSystem();
+
+};
